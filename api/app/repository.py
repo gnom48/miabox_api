14d29db56dab8d5f4343e2dd64e710f65e0f6c02
Egor Ivanov <egorchima@gmail.com>
2024-07-30 10:21:59 +0300

    files 5

--- api/app/repository.py
+++ api/app/repository.py
@@ -591,7 +591,7 @@ class Repository:
         async with new_session() as session:
             try:
                 await file.seek(0)
-                image = Image(id=0, name=new_filename, data=None)
+                image = Image(id=0, name=new_filename, data=bytearray(0))
                 new_image_id = await Repository.add_image(image, to_user_id)
                 if new_image_id is not None:
                     user = await session.get(UserOrm, to_user_id)
