acf323e7fe029f6fef9b50018b77709b39daf8a8
gnom48 <gnom.48.spb@gmail.com>
2025-10-29 13:47:25 +0300

    auth docs -> gnom48.ru/auth

--- auth/internal/server/docs/docs.go
+++ auth/internal/server/docs/docs.go
@@ -381,9 +381,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8081",
+	Host:             "gnom48.ru/auth",
 	BasePath:         "/",
-	Schemes:          []string{"http"},
+	Schemes:          []string{"https"},
 	Title:            "Auth",
 	Description:      "Account API (Auth microservice) documentation. Отвечает за авторизацию и данные о пользователях. Все остальные сервисы зависят от него, ведь именно он выпускает JWT токен и проводит интроспекцию.",
 	InfoInstanceName: "swagger",
